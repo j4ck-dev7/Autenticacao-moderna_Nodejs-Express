25b5feb417d2b3b18ed0a3d3d3c3d621ae14f319
Jackson <j4ckson7dev@gmail.com>
2026-04-01 08:55:20 -0300

    Controller | logger aplicado no getOauthUrlSignIn

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,8 +17,13 @@ export const getOauthUrlSignIn = async (req, res) => {
         const url = await OauthRequestSignIn();
         res.status(200).json({ url })
     } catch (error) {
+        const duracao = Date.now() - inicio;
+        logger.error('Erro ao gerar url para autenticação Oauth', error, {
+            usuarioId: 'Desconecido',
+            duracao
+        });
+
         res.status(500).json({ message: 'Erro ao gerar url para autenticação Oauth' })
-        console.log(error)
     }
 }
 
