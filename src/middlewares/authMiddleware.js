839e5733c662b67481534892053cbab9d3384d59
Jackson <j4ckson7dev@gmail.com>
2026-04-03 11:01:04 -0300

    authMiddleware | logger importado. Logger aplicado em casos de acesso negado, acesso autorizado e error

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,15 +1,33 @@
 import jwt from 'jsonwebtoken'
+import { logger } from '../config/logger';
 
 export const Auth = (req, res, next) => {
     const cookie = req.cookies.authenticationToken;
-    if(!cookie) return res.status(401).json({ message: 'Logue ou registre-se para acessar' })
+    if(!cookie){ 
+        logger.info('Acesso negado - token de autenticação ausente', {
+            usuarioId: 'Desconecido',
+            ip: req.ip
+        });
+
+        return res.status(401).json({ message: 'Logue ou registre-se para acessar' }) 
+    }
     
     try {
         const userVeriefied = jwt.verify(cookie, process.env.SECRET);
         req.user = userVeriefied;
+
+        logger.info('Acesso autorizado - token de autenticação válido', {
+            usuarioId: req.user._id,
+            ip: req.ip
+        });
+
         next();
     } catch (error) {
-        console.log(error)
+        logger.error('Token de autenticação inválido', error, {
+            usuarioId: 'Desconecido',
+            ip: req.ip
+        })
+
         res.status(500).json({ message: 'Erro Interno' });    
     }
 }
