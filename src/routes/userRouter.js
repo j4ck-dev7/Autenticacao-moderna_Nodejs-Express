0dc9a67537dbd69628c9536c6f2f50d2442667cc
Jackson <j4ckson7dev@gmail.com>
2026-04-16 09:23:11 -0300

    Rota para verificação de emial feita

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -13,6 +13,7 @@ router.get('/Oauth/google/get/url/signUp', slowDown.Oauth2UrlSlowDown, rateLimit
 router.get('/Oauth/signUp', slowDown.Oauth2SlowDown, rateLimit.Oauth2AuthenticationLimit, userController.signUpWithOauth);
 router.get('/Oauth/signIn', slowDown.Oauth2SlowDown, rateLimit.Oauth2AuthenticationLimit, userController.signInWithOauth);
 router.get('/main', slowDown.mainPageSlowDown, rateLimit.mainPageLimit, Auth, userController.mainPage);
+router.get('/verify-email', slowDown.verifyEmailSlowDown, rateLimit.verifyEmailLimit, userController.verifyEmail);
 router.post('/signUp', slowDown.createUserSlowDown, rateLimit.autenticacaoLimit, validate.signUpValidate, userController.signUp);
 router.post('/signIn', slowDown.authenticationSlowDown, rateLimit.autenticacaoLimit, validate.signInValidate, userController.signIn);
 router.post('/change-password', slowDown.authenticationSlowDown, rateLimit.autenticacaoLimit, Auth, validate.changePasswordValidate, userController.changePassword);
