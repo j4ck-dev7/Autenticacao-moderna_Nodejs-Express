ff6910097d71744f183d80ca32372bec3650d537
Jackson <j4ckson7dev@gmail.com>
2026-07-22 13:37:12 -0300

    Alteração de codigo no redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,10 @@
 import { createClient } from "redis";
 
-// const redisUrl = process.env.REDIS_URL || process.env.REDIS_URI || 'redis://127.0.0.1:6379';
+const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 
-export const client = createClient();
+export const client = createClient({
+  url: redisUrl,
+});
 
 client.on("error", (err) => console.error("Redis error:", err));
 client.on("connect", () => console.log("Redis connected"));
